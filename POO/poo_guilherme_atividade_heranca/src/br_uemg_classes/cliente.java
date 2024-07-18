83c5fbb8e78096030347cada360b433da2c3a09f
Guilherme Caetano <70610129+Guilherme-XHP@users.noreply.github.com>
2024-07-18 15:20:46 -0300

    atividade heranca dia 18/07/2024

---
+++ POO/poo_guilherme_atividade_heranca/src/br_uemg_classes/cliente.java
@@ -0,0 +1,31 @@
+package br_uemg_classes;
+
+public class cliente {
+	private String nome;
+    private String endereco;
+    private String telefone;
+
+    public cliente(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void imprimirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Telefone: " + telefone);
+    }
+}
